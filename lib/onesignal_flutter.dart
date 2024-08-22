c0d70a3fe9e899ff7f11b3f6d736d95b2d1e885c
hassan Al-khafagy <thief105m@gmail.com>
2024-08-22 19:31:09 +0300

    change initialize method into async

--- lib/onesignal_flutter.dart
+++ lib/onesignal_flutter.dart
@@ -45,12 +45,12 @@ class OneSignal {
   ///
   /// The initializer accepts an [appId] which the developer can get
   /// from the OneSignal consoleas well as a dictonary of [launchOptions]
-  static void initialize(String appId) {
-    _channel.invokeMethod('OneSignal#initialize', {'appId': appId});
-    InAppMessages.lifecycleInit();
-    User.lifecycleInit();
-    User.pushSubscription.lifecycleInit();
-    Notifications.lifecycleInit();
+  static Future<void> initialize(String appId) async {
+    await _channel.invokeMethod('OneSignal#initialize', {'appId': appId});
+    await InAppMessages.lifecycleInit();
+    await User.lifecycleInit();
+    await User.pushSubscription.lifecycleInit();
+    await Notifications.lifecycleInit();
   }
 
   /// Login to OneSignal under the user identified by the [externalId] provided.
